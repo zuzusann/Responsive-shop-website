3bc50300d10aad9cfec13220bc39c9be44cbe5c0
zuzusann <zuzusan65@gmail.com>
2025-01-29 03:10:39 +0630

    [REF] use module (import, export)

--- fruit-website/data/product.js
+++ fruit-website/data/product.js
@@ -1,4 +1,4 @@
-const  products = [{
+export const  products = [{
     id: 1,
     image: 'image/fruite-item-1.jpg',
     name: 'Oranges',
--- fruit-website/javascript/index.js
+++ fruit-website/javascript/index.js
@@ -1,52 +1,5 @@
-// const  products = [{
-//     image: 'image/fruite-item-1.jpg',
-//     name: '',
-//     priceCents: 1090
-//   }, {
-//     image: 'image/fruite-item-2.jpg',
-//     name: 'Oranges',
-//     priceCents: 1090
-//   }, {
-//     image: 'image/fruite-item-3.jpg',
-//     name: 'Oranges',
-//     priceCents: 1090
-//   },{
-//     image: 'image/fruite-item-4.jpg',
-//     name: 'Oranges',
-//     priceCents: 1090
-//   },{
-//     image: 'image/fruite-item-5.jpg',
-//     name: 'Oranges',
-//     priceCents: 1090
-//   },{
-//     image: 'image/fruite-item-6.jpg',
-//     name: 'Oranges',
-//     priceCents: 1090
-//   },{
-//     image: 'image/fruite-item-1.jpg',
-//     name: 'Oranges',
-//     priceCents: 1090
-//   },{
-//     image: 'image/fruite-item-2.jpg',
-//     name: 'Oranges',
-//     priceCents: 1090
-//   },{
-//     image: 'image/vegetable-item-5.jpg',
-//     name: 'Oranges',
-//     priceCents: 1090
-//   },{
-//     image: 'image/vegetable-item-4.jpg',
-//     name: 'Oranges',
-//     priceCents: 1090
-//   },{
-//     image: 'image/vegetable-item-6.jpg',
-//     name: 'Oranges',
-//     priceCents: 1090
-//   },{
-//     image: 'image/vegetable-item-5.jpg',
-//     name: 'Oranges',
-//     priceCents: 1090
-//   }]
+import { cart } from "../data/cart.js";
+import { products } from "../data/product.js";
   
   let productsHTML = '';
   
