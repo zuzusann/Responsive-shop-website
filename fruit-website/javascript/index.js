4695a5d6362a081977f96cb8d194f9dfbb33e047
zuzusann <zuzusan65@gmail.com>
2025-01-29 02:50:39 +0630

    [ADD] make cart quantity interactive

--- fruit-website/javascript/index.js
+++ fruit-website/javascript/index.js
@@ -98,7 +98,16 @@
             quantity: 1
           });
         }
-        console.log(cart);
+
+        let cartQuantity = 0;
+
+        cart.forEach((item) => {
+          cartQuantity += item.quantity;
+        });
+
+
+        document.querySelector('.js-cart-quantity').innerHTML = cartQuantity
+
       });
     });
       
